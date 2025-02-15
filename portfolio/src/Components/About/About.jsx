369220a483ad89f3048f14f901fb96638cb4a906
Swetha Murugesan <swethamk45@gmail.com>
2025-02-15 22:58:51 +0530

    updated

--- portfolio/src/Components/About/About.jsx
+++ portfolio/src/Components/About/About.jsx
@@ -6,34 +6,28 @@ const About = () => {
   return (
     <div id="about" className='about'>
         <div className='about-title'>
-            <h1>About me</h1>
+            <h1>Technical Skills & Tools</h1>
         </div>
         <div>
            <div className="about-sections">
-            <div className="about-left">
-                <img src={profile} alt=""/>
-            </div>
-            <div className="about-right">
-                <div className="about-para">
-                    <p>I am a fullstack developer with the passion of creating an end to end solution for real time problems.
-                    I always try to find the optimal and efficient solution for the problems.</p>
-                </div>
                 <div className="about-skills">
-                    <h2>Languages:</h2>
                     <div className="about-skill">
-                        <p>Html &css</p>
+                        <p>Programming Language: Java</p>
                     </div>
                     <div className="about-skill">
-                        <p>Javascript</p>
+                        <p>JavaScript</p>
                     </div>
                     <div className="about-skill">
-                        <p>React JS,Node & Express</p>
+                        <p>React JS</p>
+                    </div>
+                    
+                    <div className="about-skill">
+                        <p>PowerBI, Figma</p>
                     </div>
                     <div className="about-skill">
-                        <p>Python</p>
+                        <p>Github</p>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Hero.css';
 import profile from '../../assets/profile.jpg';
-import resume from '../../assets/resume.pdf'; 
+import resume from '../../assets/Swetha Resume.pdf'; 
+import { FaGithub,FaHackerrank,FaLinkedinIn } from 'react-icons/fa';
+import { SiLeetcode } from 'react-icons/si';
 
 const Hero = () => {
   return (
@@ -11,7 +13,21 @@ const Hero = () => {
       </div>
       <div>
         <h1><span>I'm Swetha,</span> Full stack developer from India.</h1>
-        <p>I am a passionate full-stack developer with a strong curiosity for emerging technologies. I thrive on learning and exploring the latest advancements in the field. My goal is to create end-to-end solutions that address real-world challenges effectively.</p>
+        <p>A passionate full-stack developer with a strong curiosity for emerging technologies. I thrive on learning and exploring the latest advancements in the field. My goal is to create end-to-end solutions that address real-world challenges effectively.</p>
+        <div className='hero-icons-container'>
+          <a className='hero-icons-img' href="https://github.com/SwethaMurugesan-09" target='_blank' rel="noopener noreferrer">
+              <FaGithub />
+          </a>
+          <a className='hero-icons-img' href="https://leetcode.com/u/Swetha_Murugesan/" target='_blank' rel="noopener noreferrer">
+          <SiLeetcode />
+          </a>
+          <a className='hero-icons-img' href="https://www.linkedin.com/in/swetha--murugesan/" target='_blank' rel="noopener noreferrer">
+              <FaLinkedinIn />
+          </a>
+          <a className='hero-icons-img' href="https://www.hackerrank.com/profile/swetham_22cse" target='_blank' rel="noopener noreferrer">
+              <FaHackerrank />
+          </a>
+        </div>
         <div className="hero-action">
             <div className="hero-resume">
                 <a href={resume} target="_blank" rel="noopener noreferrer">My resume</a>
