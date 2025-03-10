2677aeddc939f9c9a8644f36b9bd4c0047e99224
Swetha Murugesan <swethamk45@gmail.com>
2025-03-10 14:06:28 +0530

    updated

--- portfolio/src/assets/mywork_data.js
+++ portfolio/src/assets/mywork_data.js
@@ -2,6 +2,7 @@ import pimg1 from './pimg1.jpeg';
 import pimg2 from './pimg2.jpeg';
 import pimg3 from './pimg3.jpeg';
 import pimg4 from './pimg4.webp';
+import pimg5 from './roofings.jpg';
 const mywork_data = [
     {
         w_no: 1,
@@ -13,6 +14,14 @@ const mywork_data = [
     },
     {
         w_no: 2,
+        w_name: "Sun Roofings",
+        w_img: pimg5,
+        desc:"Sun Roofings webiste allows the user to explore the  different types of roofings materials and upvc pipes. It allows the user to add the products to toppicks and allow you to buy the products by contactin with the owner.",
+        w_link: "https://github.com/SwethaMurugesan-09/Roofings",
+        w_link1:"https://sunroofings.vercel.app"
+    },
+    {
+        w_no:3,
         w_name: "Freshmart",
         w_img: pimg2,
         desc:"Freshmart is a Grocery Management website which allow the users to buy their product by searching the categories and they can proceed by payment.",
@@ -20,9 +29,8 @@ const mywork_data = [
         w_link1:"https://freshmart-ebon.vercel.app/"
 
     },
-
     {
-        w_no: 3,
+        w_no: 4,
         w_name: "WanderWise",
         w_img: pimg3,       
          desc:"This travel planner website allows users to view the different destinations and they can book hotels and restaurants.",
@@ -30,7 +38,7 @@ const mywork_data = [
         w_link1:"https://travelplanner-theta.vercel.app/"
     },
     {
-        w_no: 4,
+        w_no: 5,
         w_name: "BeatFlow",
         w_img: pimg4,
         desc:"BeatFlow is a music webiste which allow the users to listen to their favourite songs.",
